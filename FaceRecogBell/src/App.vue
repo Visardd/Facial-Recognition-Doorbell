522ac38f36c2147c49f0c3541d83491faed60d45
Visarddd <v.neza@se21.qmul.ac.uk>
2024-04-02 22:18:11 +0100

    Revert "done it"
    This reverts commit 3bd135c319ccb49dadc657f2d186c6013695ba20.

--- FaceRecogBell/src/App.vue
+++ FaceRecogBell/src/App.vue
@@ -15,22 +15,6 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-    <div class="dropdown">
-    <button
-      class="btn btn-secondary dropdown-toggle"
-      type="button"
-      id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Check Bootstrap
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </div>
   </header>
 
   <RouterView />
